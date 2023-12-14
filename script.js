29cf19506be2bb142e3fa58df3b95409e28f7175
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-12-14 14:19:30 +0100

    Commit (miniräknare java script)

---
+++ script.js
@@ -0,0 +1,31 @@
+function plus()
+{
+let t1=parseInt(tal1.value);
+let t2=parseInt(tal2.value);
+svar = t1 + t2;
+document.querySelector("#svar").value = svar;
+}
+
+function minus()
+{
+    let t1=parseInt(tal1.value);
+    let t2=parseInt(tal2.value);
+    svar = t1 - t2;
+    document.querySelector("#svar").value = svar;
+}
+
+function times()
+{
+    let t1=parseInt(tal1.value);
+    let t2=parseInt(tal2.value);
+    svar = t1 * t2;
+    document.querySelector("#svar").value = svar;
+}
+
+function devide()
+{
+    let t1=parseInt(tal1.value);
+    let t2=parseInt(tal2.value);
+    svar = t1 / t2;
+    document.querySelector("#svar").value = svar;
+}
